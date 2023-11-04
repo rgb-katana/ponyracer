abe6e988bfca6285e0f5592ad5331078bacdf0da
KirMac <komarova1610@yandex.ru>
2023-11-04 16:02:26 +0300

    finish task-3

---
+++ src/app/races/races.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import RaceModel from '../models/race.model';
+
+@Component({
+  selector: 'pr-races',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './races.component.html',
+  styleUrls: ['./races.component.css']
+})
+export class RacesComponent {
+  races: RaceModel[] = [{ name: 'Lyon' }, { name: 'London' }];
+}
