cf6031b648c52a91f58ac6488eb56b2429685678
KirMac <komarova1610@yandex.ru>
2023-11-04 15:50:32 +0300

    finish task-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'pr-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
