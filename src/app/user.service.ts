15f0b16911ee51e7d0cad9b808b6c935fd0c888b
rgb-katana <komarova1610@yandex.ru>
2023-11-26 17:36:52 +0300

    add localstorage to store loggen in user

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,16 +10,30 @@ export class UserService {
   BASE_URL = 'https://ponyracer.ninja-squad.com';
   userEvents = new BehaviorSubject<UserModel | null>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.retrieveUser();
+  }
 
   register(login: string, password: string, birthYear: number): Observable<UserModel> {
     const body = { login, password, birthYear };
     return this.http.post<UserModel>(`${this.BASE_URL}/api/users`, body);
   }
 
+  retrieveUser() {
+    const storedUser = window.localStorage.getItem('rememberMe');
+    if (storedUser) {
+      this.userEvents.next(JSON.parse(storedUser));
+    }
+  }
+
+  storeLoggedInUser(user: UserModel) {
+    window.localStorage.setItem('rememberMe', JSON.stringify(user));
+    this.userEvents.next(user);
+  }
+
   authenticate(credentials: { login: string; password: string }): Observable<UserModel> {
     return this.http
       .post<UserModel>(`${this.BASE_URL}/api/users/authentication`, credentials)
-      .pipe(tap((user: UserModel) => this.userEvents.next(user)));
+      .pipe(tap((user: UserModel) => this.storeLoggedInUser(user)));
   }
 }
